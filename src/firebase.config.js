dd2754061a4b515b362a5f6d957917c5408692d5
medidek-dot-com <medideksm@gmail.com>
2023-10-20 16:06:27 +0530

    firebase another

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,24 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { getAuth } from "firebase/auth";
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA1lIigfq99WFR-QFc7GGx8ssHURAPkukI",
-  authDomain: "medidek-web.firebaseapp.com",
-  projectId: "medidek-web",
-  storageBucket: "medidek-web.appspot.com",
-  messagingSenderId: "821942632628",
-  appId: "1:821942632628:web:c6ab6789d3b15c863297e1"
+  apiKey: "AIzaSyBvKzm-DAeYxZJ56Ixk-A8jZ36ZyCFO2J8",
+  authDomain: "medidek-another-app.firebaseapp.com",
+  projectId: "medidek-another-app",
+  storageBucket: "medidek-another-app.appspot.com",
+  messagingSenderId: "534996524946",
+  appId: "1:534996524946:web:0a07162ba9fe2e2333eb4b"
 };
-
-
-
-
-
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
